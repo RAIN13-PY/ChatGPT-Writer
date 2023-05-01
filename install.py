c74daa4e035957815708121cdfa1f5977bbd0e6b
RAIN <64710601+RAIN13-PY@users.noreply.github.com>
2023-05-01 12:21:15 -0500

    Create install.py

---
+++ install.py
@@ -0,0 +1,10 @@
+import os
+
+# Clone the GitHub repository
+os.system("git clone https://github.com/RAIN13-PY/ChatGPT-Writer")
+
+# Navigate to the directory of the cloned repository
+os.chdir("repository")
+
+# Install the program using pip
+os.system("pip install .")
