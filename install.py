683bd28fbdff1fb98d268441c2f19aa0c35b877c
RAIN <64710601+RAIN13-PY@users.noreply.github.com>
2023-05-01 16:56:32 -0500

    Now makes a directory

--- install.py
+++ install.py
@@ -1,10 +1,12 @@
 import os
 
 # Clone the GitHub repository
+directory = os.getcwd() + "ChatGPT-Writer"
+os.mkdir(directory, 0o666)
 os.system("git clone https://github.com/RAIN13-PY/ChatGPT-Writer")
 
 # Navigate to the directory of the cloned repository
-os.chdir("repository")
+os.chdir("Chat")
 
 # Install the program using pip
 os.system("pip install .")
