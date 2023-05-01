3178423cc36ccc9325a531c7f34463f2fd5dd2f7
RAIN <64710601+RAIN13-PY@users.noreply.github.com>
2023-05-01 12:27:10 -0500

    Added packages

--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
 from setuptools import setup
 
 setup(
-    name="my_package",
+    name="ChatGPT-Writer",
     version="0.1",
     author="Your Name",
-    author_email="your.email@example.com",
-    description="My package description",
+    author_email="Not Included",
+    description="To do",
     install_requires=[
         "os",
         "pyautogui",
         "win10toast"
     ],
-    packages=["my_package"],
-    entry_points={
-        "console_scripts": [
-            "my_script = my_package.my_module:main"
-        ]
-    }
+    packages=["main", "main.makeDirectories", "main.notify"]
 )
